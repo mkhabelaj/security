0f5b1c21126f60cc4010b6a765ff3668ce77fe76
jackson <jackson@swordfish.co.za>
2018-01-06 19:38:01 +0200

    update object if settings change
    use boolean

--- customCamera.py
+++ customCamera.py
@@ -13,5 +13,14 @@ class CustomCamera(Camera, Subscriber):
         )
 
     def update(self, message):
-        print(message)
-        self.motion_detection = True
+        data = message['data']
+        self.show_video = data.get('show_video')
+        self.delta_thresh = data.get('delta_thresh')
+        self.record_motion = data.get('record_motion')
+        self.min_area = data.get('min_area')
+        self.email_images = data.get('email_images')
+        self.min_email_seconds = data.get('min_email_seconds')
+        self.motion_detection = data.get('motion_detection')
+        self.display_text_if_occupied = data.get('display_text_if_occupied')
+        self.display_text_if_unoccupied = data.get('display_text_if_unoccupied')
+        # Setting up video self.capture
--- databaseConfig/config.sql
+++ databaseConfig/config.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS config(
-    show_video INTEGER DEFAULT 1 NOT NULL,
-    record_motion INTEGER DEFAULT 0 NOT NULL,
+    show_video BOOLEAN NOT NULL DEFAULT FALSE,
+    record_motion BOOLEAN NOT NULL DEFAULT FALSE,
     min_upload_seconds INTEGER,
     min_motion_frames INTEGER,
     delta_thresh INTEGER,
-    motion_detection INTEGER  DEFAULT 0 NOT NULL,
+    motion_detection BOOLEAN NOT NULL DEFAULT TRUE,
     min_email_seconds INTEGER,
-    email_images INTEGER  DEFAULT 0 NOT NULL,
+    email_images BOOLEAN NOT NULL DEFAULT FALSE,
     min_area INTEGER NOT NULL,
     display_text_if_occupied VARCHAR(255) NOT NULL DEFAULT 'Occupied',
     display_text_if_unoccupied VARCHAR(255) NOT NULL DEFAULT 'Unoccupied'
