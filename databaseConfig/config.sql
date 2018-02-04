4cedb37948004f9f9715f4635526fd138bcf1c62
jackson <jackson@swordfish.co.za>
2018-02-04 17:30:08 +0200

    add one to one relationship to tables
    add camera name

--- databaseConfig/config.sql
+++ databaseConfig/config.sql
@@ -1,6 +1,17 @@
+DROP TABLE IF EXISTS port_map;
+
+CREATE TABLE IF NOT EXISTS port_map(
+  camera_number INTEGER NOT NULL,
+  socket_server_port INTEGER NOT NULL ,
+  websocket_server_port INTEGER NOT NULL ,
+  stream_secret VARCHAR(255) NOT NULL,
+  ip_address VARCHAR(255) NOT NULL DEFAULT 'localhost'
+);
+
 DROP TABLE IF EXISTS config;
 
 CREATE TABLE IF NOT EXISTS config(
+    camera_name VARCHAR(255) DEFAULT 'default cam name',
     show_video BOOLEAN NOT NULL DEFAULT FALSE,
     record_motion BOOLEAN NOT NULL DEFAULT FALSE,
     min_upload_seconds INTEGER,
@@ -13,25 +24,8 @@ CREATE TABLE IF NOT EXISTS config(
     resolution_width INTEGER NOT NULL,
     resolution_height INTEGER NOT NULL,
     display_text_if_occupied VARCHAR(255) NOT NULL DEFAULT 'Occupied',
-    display_text_if_unoccupied VARCHAR(255) NOT NULL DEFAULT 'Unoccupied'
+    display_text_if_unoccupied VARCHAR(255) NOT NULL DEFAULT 'Unoccupied',
+    stream_key VARCHAR(255) REFERENCES port_map (stream_key)
 );
 
-INSERT INTO config (
-  min_upload_seconds,
-  min_motion_frames,
-  delta_thresh,
-  min_email_seconds,
-  min_area,
-  resolution_width,
-  resolution_height)
-VALUES (1 ,1, 5, 3, 5000, 640, 480);
 
-DROP TABLE IF EXISTS port_map;
-
-CREATE TABLE IF NOT EXISTS port_map(
-  camera_number INTEGER NOT NULL,
-  socket_server_port INTEGER NOT NULL ,
-  websocket_server_port INTEGER NOT NULL ,
-  stream_secret VARCHAR(255) NOT NULL,
-  ip_address VARCHAR(255) NOT NULL DEFAULT 'localhost'
-);
